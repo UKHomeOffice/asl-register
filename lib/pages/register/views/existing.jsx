02922e0127f7052418f1745d95109b7a8984e12d
Joe Fitter <joefitter@hotmail.com>
2018-08-16 14:34:19 +0100

    initial commit

---
+++ lib/pages/register/views/existing.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => (
+  <div>O Hai</div>
+)
+
+export default Page;
---
+++ lib/pages/register/views/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import FormLayout from '@asl/pages/pages/common/views/layouts/form';
+import Snippet from '@asl/pages/pages/common/views/containers/snippet';
+
+const Page = ({ model: { email } }) => (
+  <FormLayout>
+    <header>
+      <h3 className="grey">Step 1 of 2</h3>
+      <h1><Snippet>createAccount</Snippet></h1>
+    </header>
+    <div className="panel panel-wide">
+      <Snippet email={email}>account.setup</Snippet>
+    </div>
+  </FormLayout>
+);
+
+const mapStateToProps = ({ model }) => ({ model });
+
+export default connect(mapStateToProps)(Page);
