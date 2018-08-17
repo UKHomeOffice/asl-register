1f7380825e5a258203753c623f29cc6299761e29
Joe Fitter <joefitter@hotmail.com>
2018-08-17 11:39:26 +0100

    pass establishment name to templates, added margin to top of header as no breadcrumbs

--- lib/pages/register/views/existing.jsx
+++ lib/pages/register/views/existing.jsx
@@ -3,11 +3,11 @@ import FormLayout from '@asl/pages/pages/common/views/layouts/form';
 import Inset from '@asl/pages/pages/common/views/components/inset';
 import Snippet from '@asl/pages/pages/common/views/containers/snippet';
 
-const Page = () => (
+const Page = ({ establishmentName }) => (
   <FormLayout>
     <header>
-      <h3>&nbsp;</h3>
-      <h1><Snippet>existingUser</Snippet></h1>
+      <h2><Snippet>existingUser</Snippet></h2>
+      <h1>{establishmentName}</h1>
     </header>
     <Inset>
       <Snippet>acceptInvitation</Snippet>
--- lib/pages/register/views/index.jsx
+++ lib/pages/register/views/index.jsx
@@ -3,11 +3,16 @@ import { connect } from 'react-redux';
 import FormLayout from '@asl/pages/pages/common/views/layouts/form';
 import Snippet from '@asl/pages/pages/common/views/containers/snippet';
 
-const Page = ({ model: { email } }) => (
+const Page = ({
+  establishmentName,
+  model: {
+    email
+  }
+}) => (
   <FormLayout>
     <header>
-      <h3>&nbsp;</h3>
-      <h1><Snippet>createAccount</Snippet></h1>
+      <h2><Snippet>createAccount</Snippet></h2>
+      <h1>{establishmentName}</h1>
     </header>
     <div className="panel panel-wide">
       <Snippet email={email}>account.setup</Snippet>
